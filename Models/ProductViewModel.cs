7377210b55619bd90d42860cb02390664d47b6be
SasVel <sasvelchev@gmail.com>
2022-10-19 19:15:27 +0300

    Add project files.

---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ExApp.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
